b262b38d63a03e3045ad20ccab6a887ec3e0f2f5
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2020-01-15 13:01:32 +0300

    Updates

---
+++ precache-manifest.aa69ed971bec67ad496cb6646cad3fa7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3869dc9cbbeb748990b4c59e75b68fbe",
+    "url": "/react-tech-store/index.html"
+  },
+  {
+    "revision": "6fc30b92f568e97da760",
+    "url": "/react-tech-store/static/css/main.578199cb.chunk.css"
+  },
+  {
+    "revision": "21577292a05e915ca82f",
+    "url": "/react-tech-store/static/js/2.f7e28418.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-tech-store/static/js/2.f7e28418.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6fc30b92f568e97da760",
+    "url": "/react-tech-store/static/js/main.66e30c58.chunk.js"
+  },
+  {
+    "revision": "09a6f875561754b6194e",
+    "url": "/react-tech-store/static/js/runtime-main.e06ecd75.js"
+  },
+  {
+    "revision": "7ac3ba3ab8503352edfc941d7ecd430d",
+    "url": "/react-tech-store/static/media/logo.7ac3ba3a.svg"
+  },
+  {
+    "revision": "016d4fcc158e863ea29afc6b6ff6c3ff",
+    "url": "/react-tech-store/static/media/mainBcg.016d4fcc.jpeg"
+  }
+]);
