f1c610a2842471fe0582c3485d5daeeb0da3e315
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2020-01-15 12:58:30 +0300

    create the router and nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from "react";
-
+//react router dom
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+//Pages
+import About from './pages/About';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import ProductDetails from './pages/ProductDetails';
+import Products from './pages/Products';
+//components
+import Header from './components/Header'
 function App() {
-  return <h1>hello from app component</h1>;
+  return <Router>
+    <Header />
+    <Switch>
+      <Route exact path="/"><Home /></Route>
+      <Route path="/about"><About /></Route>
+      <Route path="/cart"><Cart /></Route>
+      <Route path="/checkout"><Checkout /></Route>
+      <Route path="/login"><Login /></Route>
+      <Route exact path="/products"><Products /></Route>
+      <Route path="/products/:id" children={<ProductDetails />}></Route>
+      <Route path="*"><Error /></Route>
+    </Switch>
+  </Router>;
 }
 
 export default App;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,8 @@
 import React from "react";
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return <section className="section about-section">
+    <h1 className="section-title">about us</h1>
+    <p>Gubergren stet rebum voluptua lorem nonumy stet et ipsum sea labore. Lorem sed labore et sanctus sit sanctus, sit eirmod Gubergren stet rebum voluptua lorem nonumy stet et ipsum sea labore. Lorem sed labore et sanctus sit sanctus, sit eirmod Gubergren stet rebum voluptua lorem nonumy stet et ipsum sea labore. Lorem sed labore et sanctus sit sanctus, sit eirmod Gubergren stet rebum voluptua. </p>
+  </section>;
 }
